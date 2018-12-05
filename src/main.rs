294d5602ad2fc8ee24366d40489904b8554e8893
Samuel Leeuwenburg <samleeuwenburg@gmail.com>
2018-12-05 20:29:56 +0100

    finish day1

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,55 @@ use std::fs::File;
 use std::path::Path;
 use std::error::Error;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     day1();
 }
 
-
 fn day1() {
     let data = open_file(Path::new("./input/day1.txt"));
     let mut iter = data.split_whitespace();
-
     let mut frequency = 0i32;
+    let mut frequencies = HashSet::new();
+    let mut first_repeat : Option<i32> = None;
+    let mut loops = 0i32;
+
     loop {
         match iter.next() {
-            Some(word) => {
-                match word.parse::<i32>() {
-                    Err(why) => panic!("{} : {}", word, why),
-                    Ok(i) => frequency += i
+            Some(number) => {
+                match number.parse::<i32>() {
+                    Ok(i) => {
+                        frequency += i;
+
+                        if frequencies.contains(&frequency) {
+                            first_repeat = Some(frequency);
+                            break;
+                        }
+
+                        frequencies.insert(frequency);
+                    }
+                    Err(why) => panic!("{} : {}", number, why)
                 }
             }
-            None => break
+            None => {
+                if loops > 1000 {
+                    break;
+                }
+
+                loops += 1;
+
+                iter = data.split_whitespace();
+            }
         }
     }
-    
-    println!("{}", frequency)
+
+    match first_repeat {
+        Some(i) => println!("first repeat was {}, found after repeating the input {} times", i, loops),
+        None => println!("no repeat found"),
+    }
 }
+
 
 fn open_file(path : &Path) -> String {
     let mut file = match File::open(&path) {
